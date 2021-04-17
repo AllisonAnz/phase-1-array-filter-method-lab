552633b46aed455d4e56e18460f17258132e10a3
AllisonAnz <AllisonAnzalone@gmail.com>
2021-04-17 13:20:37 -0500

    commit tree 4d54564805bcd53eaf8d2d96f52fda160921f0df

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+// findMatch() returns matching drivers if case does not match but letters do 
+function findMatching(drivers, driverName) {
+    return drivers.filter(function(name) {
+        return name.toLowerCase() === driverName.toLowerCase()
+    })
+}
+
+// fuzzyMatch() returns driver if beginning provided letter match 
+// does not return drivers if only middle of ending letters match 
+function fuzzyMatch(drivers, letterMatch) {
+    return drivers.filter(function (match) {
+        return match.toLowerCase().indexOf(letterMatch.toLowerCase()) === 0
+    })
+}
+
+// MatchName function takes an array of drivers objects and a string as arguments. 
+// Each driver object has two properties: name and hometown. 
+// The function should return each element whos name property matches the provided string argument. 
+function matchName(drivers, driverName){
+    return drivers.filter(record => record.name === driverName)
+}
+
